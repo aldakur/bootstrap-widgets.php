5fe3ed89cb87c3e345c6e3adb808b89b07c02eed
Bryan Willis <willis.bryan@live.com>
2015-12-13 14:56:58 -0600

    Update bootstrap-widget-filters.php

--- bootstrap-widget-filters.php
+++ bootstrap-widget-filters.php
@@ -11,7 +11,7 @@
  * @wordpress-plugin
  * Plugin Name:       Wordpress Widget Filters
  * Plugin URI:        https://github.com/Wordpress-Development/bootstrap-widgets.php
- * Description:       Add Bootstrap to wordpress widgets. Widget Output Filters plugin included.
+ * Description:       Add Bootstrap to wordpress widgets. Widget Output Filters plugin included. Requires Widget Output Filters Plugin and theme with Bootstrap 3 support.
  * Version:           1.0.0
  * Author:            Bryan Willis
  * Author URI:        http://profiles.wordpress.org/codecandid
@@ -20,10 +20,16 @@
  */
 
 
+/**
+ * This plugin does the following on activation:
+ * - Checks if Widget Output Filters is activated and if not deactivate itself.
+ * - Filters the html markup to support Bootstrap 3 styling. Without Botstrap enqueued it won't appear to do anything.
+ */
+
 defined( 'WPINC' ) or die;
 
-register_activation_hook( __FILE__, 'activate_wop_bootstrap_plugin_script_check' );
-function activate_wop_bootstrap_plugin_script_check() {
+register_activation_hook( __FILE__, 'activate_wop_bootstrap_register_activation_hook' );
+function activate_wop_bootstrap_register_activation_hook() {
 	if ( ! function_exists('widget_output_filters_dynamic_sidebar_params') ) {
 		deactivate_plugins( plugin_basename( __FILE__ ) );
 		wp_die( sprintf( __( 'Sorry, you can\'t activate %1$sBootstrap-Widget-Filters unless you have installed the %3$sWidget Output Filters Plugin%4$s. Go back to the %5$sPlugins Page%4$s.' ), '<em>', '</em>', '<a href="https://wordpress.org/plugins/widget-output-filters/" target="_blank">', '</a>', '<a href="javascript:history.back()">' ) );
@@ -32,49 +38,58 @@ function activate_wop_bootstrap_plugin_script_check() {
 
 
 
+
+
 /** 
- * Bootstrap support for core wordpress widgets
+ * Bootstrap 3 support for core wordpress widgets
  */
 function brw_bootstrap_widget_output_filters( $widget_output, $widget_type, $widget_id ) {
-	if ( 'categories' == $widget_type ) {
-      		$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
-      		$widget_output = str_replace('<li class="cat-item cat-item-', '<li class="list-group-item cat-item cat-item-', $widget_output);
-      		$widget_output = str_replace('(', '<span class="badge cat-item-count"> ', $widget_output);
-      		$widget_output = str_replace(')', ' </span>', $widget_output);
-    	}
-    	elseif ( 'calendar' == $widget_type ) {
-		$widget_output = str_replace('calendar_wrap', 'calendar_wrap table-responsive', $widget_output);
-        	$widget_output = str_replace('<table id="wp-calendar', '<table class="table table-condensed" id="wp-calendar', $widget_output);
-    	}
-    	elseif ( 'tag_cloud' == $widget_type )  {
-		$regex = "/(<a[^>]+?)( style='font-size:.+pt;'>)([^<]+?)(<\/a>)/";
-		$replace_with = "$1><span class='label label-primary'>$3</span>$4";
-		$widget_output = preg_replace( $regex , $replace_with , $widget_output );
-	}
-  	elseif ( 'archives' == $widget_type ) {
-      		$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
-      		$widget_output = str_replace('<li>', '<li class="list-group-item archive-list-group-item">', $widget_output);
-		$widget_output = str_replace('(', '<span class="badge cat-item-count"> ', $widget_output);
-   		$widget_output = str_replace(')', ' </span>', $widget_output);
-   	}
-  	elseif ( 'meta' == $widget_type ) {
-        	$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
-        	$widget_output = str_replace('<li>', '<li class="list-group-item meta-list-group-item">', $widget_output);
-   	}
-  	elseif ( 'recent-posts' == $widget_type ) {
-        	$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
-        	$widget_output = str_replace('<li>', '<li class="list-group-item recent-posts-list-group-item">', $widget_output);
-   	}
-  	elseif ( 'recent-comments' == $widget_type ) {
-        	$widget_output = str_replace('<ul id="recentcomments">', '<ul id="recentcomments" class="list-group">', $widget_output);
-        	$widget_output = str_replace('<li class="recentcomments">', '<li class="recentcomments list-group-item recent-comments-list-group-item">', $widget_output);
-   	}
-  	elseif ( 'pages' == $widget_type ) {
-        	$widget_output = str_replace('<ul>', '<ul class="nav nav-stacked nav-pills">', $widget_output);
-   	}
-  	elseif ( 'nav_menu' == $widget_type ) {
-        	$widget_output = str_replace(' class="menu"', 'class="menu nav nav-stacked nav-pills"', $widget_output);
-   	}
+	
+	switch( $widget_type ) {
+		
+		case 'categories' :
+      			$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
+      			$widget_output = str_replace('<li class="cat-item cat-item-', '<li class="list-group-item cat-item cat-item-', $widget_output);
+      			$widget_output = str_replace('(', '<span class="badge cat-item-count"> ', $widget_output);
+      			$widget_output = str_replace(')', ' </span>', $widget_output);
+      			break;
+		case 'calendar' :
+			$widget_output = str_replace('calendar_wrap', 'calendar_wrap table-responsive', $widget_output);
+        		$widget_output = str_replace('<table id="wp-calendar', '<table class="table table-condensed" id="wp-calendar', $widget_output);
+    			break;
+		case 'tag_cloud' :    	
+			$regex = "/(<a[^>]+?)( style='font-size:.+pt;'>)([^<]+?)(<\/a>)/";
+			$replace_with = "$1><span class='label label-primary'>$3</span>$4";
+			$widget_output = preg_replace( $regex , $replace_with , $widget_output );
+    			break;
+		case 'archives' :	
+      			$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
+      			$widget_output = str_replace('<li>', '<li class="list-group-item archive-list-group-item">', $widget_output);
+			$widget_output = str_replace('(', '<span class="badge cat-item-count"> ', $widget_output);
+   			$widget_output = str_replace(')', ' </span>', $widget_output);
+    			break;
+		case 'meta' :   	
+        		$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
+        		$widget_output = str_replace('<li>', '<li class="list-group-item meta-list-group-item">', $widget_output);
+    			break;
+		case 'recent-posts' :   	
+        		$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
+        		$widget_output = str_replace('<li>', '<li class="list-group-item recent-posts-list-group-item">', $widget_output);
+    			break;
+		case 'recent-comments' :   	
+        		$widget_output = str_replace('<ul id="recentcomments">', '<ul id="recentcomments" class="list-group">', $widget_output);
+        		$widget_output = str_replace('<li class="recentcomments">', '<li class="recentcomments list-group-item recent-comments-list-group-item">', $widget_output);
+     			break;
+		case 'pages' :   	
+	        	$widget_output = str_replace('<ul>', '<ul class="nav nav-stacked nav-pills">', $widget_output);
+	     		break;
+		case 'nav_menu' :   	
+	        	$widget_output = str_replace(' class="menu"', 'class="menu nav nav-stacked nav-pills"', $widget_output);
+	    		break;
+    		default:
+			$widget_output = $widget_output; // not sure if this is needed
+			
       return $widget_output;
+      
 }
 add_filter( 'widget_output', 'brw_bootstrap_widget_output_filters', 10, 3 );
